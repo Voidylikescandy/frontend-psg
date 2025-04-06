6fafc9510e0049299078465405ebce95fe33bef4
Voidylikescandy <jaydyuv@gmail.com>
2025-04-06 17:44:16 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-const API_URL = 'https://psg-backend.loca.lt';
+// This should be updated with your ngrok URL when testing with Vercel frontend
+const API_URL = 'https://ympfqs62afgf.share.zrok.io';
 export default API_URL;
