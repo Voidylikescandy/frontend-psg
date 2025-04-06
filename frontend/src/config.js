588d0b2ce120461f64da08f41193184799208bf9
Voidylikescandy <jaydyuv@gmail.com>
2025-04-06 18:06:46 +0530

    test

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // This should be updated with your ngrok URL when testing with Vercel frontend
-const API_URL = 'https://ympfqs62afgf.share.zrok.io';
+const API_URL = 'https://4b6f-34-82-29-127.ngrok-free.app/';
 export default API_URL;
